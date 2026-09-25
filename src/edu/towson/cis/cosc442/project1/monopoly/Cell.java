871a87aa5c8bc27e760a90c44fbff04029f0b71a
Jesse Rosas <135461760+JesseRosas@users.noreply.github.com>
2026-09-25 16:22:06 -0400

    Refactor Cell.java

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -2,40 +2,64 @@ package edu.towson.cis.cosc442.project1.monopoly;
 
 public abstract class Cell {
 	private boolean available = true;
-	private String name;
-	protected Player owner;
+	private String theOwner;
+	protected Player theOwner;
 
+	/** 
+	 * @return String
+	 */
 	public String getName() {
-		return name;
+		return theOwner;
 	}
 
-	public Player getOwner() {
-		return owner;
+	/** 
+	 * @return Player
+	 */
+	public Player getTheOwner() {
+		return theOwner;
 	}
 	
+	/** 
+	 * @return int
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/** 
+	 * @return boolean
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
 	public abstract void playAction();
 
+	/** 
+	 * @param available
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/** 
+	 * @param name
+	 */
 	void setName(String name) {
-		this.name = name;
+		this.theOwner = name;
 	}
 
-	public void setOwner(Player owner) {
-		this.owner = owner;
+	/** 
+	 * @param owner
+	 */
+	public void setTheOwner(Player owner) {
+		this.theOwner = owner;
 	}
     
-    public String toString() {
-        return name;
+    /** 
+	 * @return String
+	 */
+	public String toString() {
+        return theOwner;
     }
 }
